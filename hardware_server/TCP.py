7d0ff92346633d5c1d1a74e53189c03d01494428
Donald Jennings <D.D.Jennings@outlook.com>
2023-03-06 14:54:14 +0000

    added docstrings to the TCP

--- hardware_server/TCP.py
+++ hardware_server/TCP.py
@@ -1,3 +1,7 @@
+"""
+
+"""
+
 import socket
 import struct
 import threading
@@ -14,6 +18,9 @@ class TCP:
     PORT = 5000
 
     def __init__(self):
+        """
+        Initializes the TCP server.
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # prevents OSError Address already in use exception
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -23,6 +30,10 @@ class TCP:
         self.receive_lock = threading.Lock()
 
     def _send_thread(self):
+        """
+        Sends data from the send queue to the client.
+        :return:
+        """
         while True:
             if self.send_queue:
                 data_type, data = self.send_queue.get()
@@ -30,6 +41,10 @@ class TCP:
                 self.socket.sendall(packet_bytes)
 
     def _receive_thread(self):
+        """
+        Receives data from the client and adds it to receive queue.
+        :return:
+        """
         while True:
             header_bytes = self.socket.recv(Header.HEADER_LEN)
             if not header_bytes:
@@ -37,21 +52,32 @@ class TCP:
 
             header = Header.from_bytes(header_bytes)
             data_bytes = self.socket.recv(header.data_len)
-            # TODO: implement dataclass Data to be able to use this:
-            #  data = Data.from_bytes(data_bytes)
             data = struct.unpack("!ii", data_bytes)
 
             with self.receive_lock:
                 self.receive_queue.append(data)
 
     def receive_data(self):
+        """
+        Returns a copy of receive queue.
+        :return: copy of receive queue
+        """
         with self.receive_lock:
             return self.receive_queue.copy()
 
     def send_data(self, data):
+        """
+        Adds data to send queue.
+        :param data:
+        :return:
+        """
         self.send_queue.put(data)
 
     def run(self):
+        """
+        Starts the server.
+        :return:
+        """
         self.socket.listen(1)
         print("Waiting for a connection...")
 
